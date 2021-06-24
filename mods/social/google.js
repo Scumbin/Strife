447206de12152beca23fb8caf5e0f9059210f49d
Scumbin <>
2021-06-24 18:04:14 -0400

    perhaps thi s fixes

--- mods/social/google.js
+++ mods/social/google.js
@@ -1,15 +1,16 @@
 module.exports = {
 	command: "google",
-	description: "send a google link (LMGTFY but you want to keep your friendship)",
+	description:
+		"send a google link (LMGTFY but you want to keep your friendship)",
 	usage: "{c} [thing to search]",
-	executor: this.handleCommand.bind(this),
-	handleCommand(args) {
+	category: "Google",
+	executor(args) {
 		let result = encodeURIComponent(args)
 			.replace(/'/g, "%27")
 			.replace(/"/g, "%22");
 		return {
 			send: true,
 			result: `<https://lmgtfy.com/?${result}>`,
-		}
-	}
-}
+		};
+	},
+};
